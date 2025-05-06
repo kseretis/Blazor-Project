47e0a7731e1b0f7d6612b1c07b075bf8710941f7
k.seretis <serkleanthis@gmail.com>
2025-05-06 22:43:28 +0300

    Fixes

--- BlazorApp.Client/Program.cs
+++ BlazorApp.Client/Program.cs
@@ -1,9 +1,5 @@
-using BlazorApp.Client.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-builder.Services.AddSingleton<TokenService>();
-builder.Services.AddScoped<CustomerApiService>();
 
 await builder.Build().RunAsync();
